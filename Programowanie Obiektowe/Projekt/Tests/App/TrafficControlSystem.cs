d75bf4e004be0fc08f76a9c37b3d93b668190f32
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-07-08 23:25:27 +0200

    Repository refresh

--- Programowanie Obiektowe/Projekt/Tests/App/TrafficControlSystem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace TrafficControlSystem
-{
-    class IncorrectPosition : Exception
-    {
-        public IncorrectPosition() : base("Wrong Value for Position.") { }
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# Studies-Engineer-2-Semester-PL
-Second semester of studies at Bialystok Technical University.
-
-This folder contains programs in Polish and English.
